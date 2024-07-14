4f5f51f7626b64a9141cade40b12f5d450b3e25f
MatheusJunior2334 <matheusjuniormjg2334@gmail.com>
2024-07-14 01:17:20 -0300

    fix: scrollToTop bug fixed (try 1)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,17 +1,13 @@
-import dynamic from "next/dynamic";
 import { Metadata } from "next";
 import { LanguageProvider } from "@/app/contexts/languageContext";
 import { MainLayout } from "../layout/mainLayout";
 
+import { TopBackgroundSection } from "../components/about/topBackgroundSection";
 import { MainSectionAbout } from "../components/about/mainSection";
+import { AnaloguePhotography } from "../components/about/analoguePhotography";
+
 import { ProjectMembers } from "../components/about/projectMembers";
 
-const TopBackgroundSection = dynamic(() => import("../components/about/topBackgroundSection").then(mod => mod.TopBackgroundSection), {
-    ssr: true
-})
-const AnaloguePhotography = dynamic(() => import("../components/about/analoguePhotography").then(mod => mod.AnaloguePhotography), { 
-    ssr: false 
-})
 
 export const metadata: Metadata = {
     title: 'About',
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,22 +1,15 @@
 import React from "react";
-import dynamic from "next/dynamic";
 
 import { MainSection } from "../components/home/mainSection";
 import { OurWorkSection } from "../components/home/ourWork";
 import { HomeBlogPosts } from "../components/home/homeBlogPosts";
 import { Section4 } from "../components/home/section4";
 import { AboutSection } from "../components/home/about";
+import { CorePrinciples } from "../components/home/corePrinciples";
+import { Testimonials } from "../components/home/testimonials";
 
 import { LanguageProvider } from "../contexts/languageContext";
 import { MainLayout } from "../layout/mainLayout";
-
-const CorePrinciples = dynamic(() => import('../components/home/corePrinciples').then(mod => mod.CorePrinciples), {
-    ssr: false
-})
-
-const Testimonials = dynamic(() => import('../components/home/testimonials').then(mod => mod.Testimonials), {
-    ssr: false
-})
 
 export default function HomePage() {
     return (
