da9948fd75ab7be0b7afb82802552142bb97ee00
MatheusJunior2334 <matheusjuniormjg2334@gmail.com>
2024-07-21 23:07:15 -0300

    fix: fixing deploy error (try 2)

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -20,7 +20,7 @@ declare module 'next-auth/jwt' {
   }
 }
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
