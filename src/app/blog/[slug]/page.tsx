a7ad3f623aefd01f0d173d5002e1bb9b42af4e5b
MatheusJunior2334 <matheusjuniormjg2334@gmail.com>
2025-05-02 00:54:42 -0300

    fix: fixing deploy error (try 6)

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,25 +3,23 @@ import BlogPost from "@/components/blog/blogPost";
 import { notFound } from "next/navigation";
 import { Metadata } from "next";
 
-interface PageProps {
-    params: {
-        slug: string;
-    }
-}
+type Params = Promise<{ slug: string }>
 
 export async function generateStaticParams() {
     return blogPosts.map((post) => ({ slug: post.slug }))
 }
 
-export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
-    const post = blogPosts.find((p) => p.slug === params.slug);
+export async function generateMetadata({ params }: { params: Params }): Promise<Metadata> {
+    const { slug } = await params;
+    const post = blogPosts.find((p) => p.slug === slug);
     return {
         title: post ? post.title : "Post não encontrado"
     }
 }
 
-export default function BlogPostPage({ params }: PageProps){
-    const post = blogPosts.find(p => p.slug === params.slug);
+export default async function BlogPostPage({ params }: { params: Params }){
+    const { slug } = await params;
+    const post = blogPosts.find((p) => p.slug === slug);
 
     if (!post) return notFound();
 
