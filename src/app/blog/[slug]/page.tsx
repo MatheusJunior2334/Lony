4e83274ccc368a88b74124867a3ca605d7f2b6ba
MatheusJunior2334 <matheusjuniormjg2334@gmail.com>
2025-05-02 00:44:51 -0300

    fix: fixing deploy error (try 5)

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,7 +3,7 @@ import BlogPost from "@/components/blog/blogPost";
 import { notFound } from "next/navigation";
 import { Metadata } from "next";
 
-type Props = {
+interface PageProps {
     params: {
         slug: string;
     }
@@ -13,14 +13,14 @@ export async function generateStaticParams() {
     return blogPosts.map((post) => ({ slug: post.slug }))
 }
 
-export function generateMetadata({ params }: Props): Metadata {
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
     const post = blogPosts.find((p) => p.slug === params.slug);
     return {
         title: post ? post.title : "Post não encontrado"
     }
 }
 
-export default function BlogPostPage({ params }: Props){
+export default function BlogPostPage({ params }: PageProps){
     const post = blogPosts.find(p => p.slug === params.slug);
 
     if (!post) return notFound();
